45d31eb9ad3d7666e5fa1c1166302ecdc2ad5cd7
Sofia <sofyomg26@gmail.com>
2023-12-07 11:08:05 -0300

    Servidor express

---
+++ BACKEND/index.js
@@ -0,0 +1,32 @@
+
+const express = require('express')
+const server = express()
+
+
+const PORT = 3000
+
+server.get('/', (req, res) => {
+  res.send('Hola Mundo!')
+})
+
+//probando status code
+
+/* 
+server.get('/ruta2', (req, res) => {
+    const ruta = true;
+    
+    if (ruta){
+      res.send('Segunda ruta aqui'); //por defecto el status 200
+    } else {
+      res.status(404).send('Status 404: No se encontro la ruta');
+    }
+  });
+  */
+
+
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+
+})
+
